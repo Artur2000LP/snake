4662fbecd7bcfe2427c480406e505aa8c7d4b990
OscarDevCPP <oscarchh0799@gmail.com>
2024-08-01 18:16:17 -0500

    empty message

--- src/main/java/com/artur/Main.java
+++ src/main/java/com/artur/Main.java
@@ -2,6 +2,6 @@ package com.artur;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola a totos este es mi primer trabajo aprendiendo git  ");
+        System.out.println("");
     }
 }
