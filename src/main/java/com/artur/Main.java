d0a6a1b3bff56126f097ac1cff12eaffdd06996d
Artur2000LP <letonaporrasarturo@gmail.com>
2024-08-01 00:24:42 -0500

    agrega como estan todos

--- src/main/java/com/artur/Main.java
+++ src/main/java/com/artur/Main.java
@@ -2,6 +2,6 @@ package com.artur;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo como estan todos ");
     }
 }
