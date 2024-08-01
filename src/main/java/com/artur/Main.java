a0bc1cc2f94d059aa500746e8d44218ad1475c6a
Artur2000LP <letonaporrasarturo@gmail.com>
2024-08-01 15:29:17 -0500

    se actualizo el mensaje a aprendiendo git

--- src/main/java/com/artur/Main.java
+++ src/main/java/com/artur/Main.java
@@ -2,6 +2,6 @@ package com.artur;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo como estan todos ");
+        System.out.println("hola a totos este es mi primer trabajo aprendiendo git  ");
     }
 }
